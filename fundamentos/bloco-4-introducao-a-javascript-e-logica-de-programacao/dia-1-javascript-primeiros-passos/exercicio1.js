bf97a77ec6535062821b50c0e712e2dd67042ca7
João M <joaomarcelofg@hotmail.com>
2022-02-02 15:42:08 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,9 @@
+//Exercicio 1
+const a = 50;
+const b = 10;
+
+console.log("a + b e igual a:", a + b)
+console.log("a - b e igual a:", a - b)
+console.log("a vezes b e igual a:", a * b)
+console.log("a dividido por b e igual a:", a / b)
+console.log("o modulo e 'a' sobre 'b' e igual a:", a % b)
