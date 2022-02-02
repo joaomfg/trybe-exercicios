fcb54bf64fc9117b56271dcc6b8d897ab8b1d427
João M <joaomarcelofg@hotmail.com>
2022-02-02 15:52:38 -0300

    exercicio 2 e 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,8 @@
+const a = 100;
+const b = 11;
+
+if (a > b) {
+    console.log("'a' e maior que 'b'", "(" + a, ">", b + ")")
+} else {
+    console.log("'b' e maior que 'a'", "(" + b, ">", a + ")")
+}
