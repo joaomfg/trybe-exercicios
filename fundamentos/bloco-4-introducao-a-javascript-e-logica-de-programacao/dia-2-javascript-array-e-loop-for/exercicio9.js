3329aa88aca533955d189b7da7db73427ec8227f
João M <joaomarcelofg@hotmail.com>
2022-02-03 16:50:30 -0300

    exercicio 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
@@ -0,0 +1,15 @@
+let numbers = [];
+
+for (i = 1; i <= 25; i += 1) {
+    numbers.push(i)
+}
+
+console.log(numbers)
+
+let division = []
+
+for (i = 0; i < numbers.length; i += 1) {
+    division.push(numbers[i] / 2)
+}
+
+console.log(division)
