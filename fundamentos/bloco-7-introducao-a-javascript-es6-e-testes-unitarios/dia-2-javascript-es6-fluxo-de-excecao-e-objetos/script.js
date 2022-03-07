3e0f628a8b2b0a2573509695aec4ac7bff791a79
João M <joaomarcelofg@hotmail.com>
2022-03-07 20:05:26 -0300

    parte 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -34,53 +34,130 @@
 //     button.addEventListener('click', sum);
 //   }
 
-// Parte 2:
-const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
-      },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
-    },
-    payment: {
-      total: 60,
-    },
+// // Parte 2:
+// const order = {
+//     name: 'Rafael Andrade',
+//     phoneNumber: '11-98763-1416',
+//     address: {
+//       street: 'Rua das Flores',
+//       number: '389',
+//       apartment: '701',
+//     },
+//     order: {
+//       pizza: {
+//         marguerita: {
+//           amount: 1,
+//           price: 25,
+//         },
+//         pepperoni: {
+//           amount: 1,
+//           price: 20,
+//         }
+//       },
+//       drinks: {
+//         coke: {
+//           type: 'Coca-Cola Zero',
+//           price: 10,
+//           amount: 1,
+//         }
+//       },
+//       delivery: {
+//         deliveryPerson: 'Ana Silveira',
+//         price: 5,
+//       }
+//     },
+//     payment: {
+//       total: 60,
+//     },
+//   };
+  
+//   const customerInfo = (order) => {
+//     console.log(`Ola ${order.order.delivery.deliveryPerson}, entrega para ${order.name}, telefone: ${order.phoneNumber}, ${order.address.street}, N: ${order.address.number}, AP: ${order.address.apartment}`);
+  
+//   }
+  
+//   customerInfo(order);
+  
+//   const orderModifier = (order) => {
+//     // Adicione abaixo as informações necessárias.
+  
+//   }
+  
+//   orderModifier(order);
+
+// Parte 3:
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
   };
   
-  const customerInfo = (order) => {
-    console.log(`Ola ${order.order.delivery.deliveryPerson}, entrega para ${order.name}, telefone: ${order.phoneNumber}, ${order.address.street}, N: ${order.address.number}, AP: ${order.address.apartment}`);
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
   
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const changeObj = (obj, key, value) => {
+    return obj[key] = value;
   }
-  
-  customerInfo(order);
-  
-  const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
+  changeObj(lesson2, 'turno', 'noite');
+
+  ////////////////////////////////
+
+  const objKeys = (obj) => {
+    return Object.keys(obj); 
   }
-  
-  orderModifier(order);
+  console.log(objKeys(lesson1))
+
+    ////////////////////////////////
+
+  function objSize(obj) {
+    return Object.keys(obj).length
+  }
+  console.log(objSize(lesson1));
+
+  ////////////////////////////////
+
+  const allLessons = {lesson1, lesson2, lesson3}
+  Object.assign({}, allLessons);
+  console.log(allLessons);
+
+  ////////////////////////////////
+
+  function students(obj) {
+    return allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes + allLessons.lesson3.numeroEstudantes
+  }
+  console.log(students(allLessons));
+
+    ////////////////////////////////
+
+  function getValue(obj, key) {
+    const oi = Object.values(obj);
+    return oi[key]
+  }
+  console.log(getValue(lesson1, 2));
+
+    ////////////////////////////////
+
+  function verifyObj(obj, key, value) {
+    const arr = Object.entries(obj);
+    let result = false;
+
+    for (let i in arr) {
+        if (arr[i][0] === key && arr[i][1] === value) {
+            result = true;
+        }
+    }
+    return result
+  }
+  console.log(verifyObj(lesson3, 'turno', 'noite'));
